7d6558d37cdc1c180ab82bce5c3c51777d0a0a13
Dan Edington <danedington@Dans-MBP.lan>
2019-01-15 15:20:09 +0000

    tidied up

--- src/index.js
+++ src/index.js
@@ -10,18 +10,18 @@ const rl = readline.createInterface({
 
 let score = 0
 let count = 0
-const notes = _.shuffle(Tonal.Scale.notes('c chromatic'))
-const scale = Tonal.Scale.notes(`${notes[0]} major`)
+const scaleName = _.shuffle(Tonal.Scale.notes('c chromatic'))[0]
+const scale = Tonal.Scale.notes(`${scaleName} major`)
 
 const checkAnswer = answer => {
 
-    answer = _.trim(_.toLower(answer))
+    const correctNote = scale[0]
 
-    if (answer === _.toLower(scale[0])) {
+    if (_.trim(_.toLower(answer)) === _trim(_.toLower(correctNote))) {
         console.log(chalk.bgGreen.white('CORRECT!\r\n'))
         score++
     } else {
-        console.log(chalk.bgRed.white(`WRONG! The correct answer is ${scale[0]}\r\n`))
+        console.log(chalk.bgRed.white(`WRONG! The correct answer is ${correctNote}\r\n`))
     }
 
     scale.shift()
@@ -33,7 +33,7 @@ const askQuestion = async () => {
 
     return new Promise((resolve) => {
 
-        rl.question(chalk.bgYellow.black(`What is note ${count+1} of the ${notes[0]} major scale?\r\n`), (answer) => {
+        rl.question(chalk.bgYellow.black(`What is note ${count+1} of the ${scaleName} major scale?\r\n`), (answer) => {
             checkAnswer(answer)
             resolve()
         })
@@ -50,8 +50,7 @@ const askQuestion = async () => {
 
     }
 
-    let message = `You got ${score}/7 correct.`
-    console.log(chalk.bgYellow.black(message))
+    console.log(chalk.bgYellow.black(`You got ${score}/7 correct.`))
     rl.close();
 
 })()
