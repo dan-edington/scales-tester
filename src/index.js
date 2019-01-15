4df4956fc40de967168ca9d0c21b6d0f5d1662f1
Dan Edington <danedington@Dans-MBP.lan>
2019-01-15 17:47:53 +0000

    added emojis

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const chalk = require('chalk')
 const Tonal = require('tonal')
 const _ = require('lodash')
 const readline = require('readline');
+const emoji = require('node-emoji')
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -16,12 +17,14 @@ const scale = Tonal.Scale.notes(`${scaleName} major`)
 const checkAnswer = answer => {
 
     const correctNote = scale[0]
+    const tada = emoji.get('tada')
+    const sad = emoji.get('sob')
 
     if (_.trim(_.toLower(answer)) === _.trim(_.toLower(correctNote))) {
-        console.log(chalk.bgGreen.white('CORRECT!\r\n'))
+        console.log(chalk.bgGreen.white(` ${tada} CORRECT! \r\n`))
         score++
     } else {
-        console.log(chalk.bgRed.white(`WRONG! The correct answer is ${correctNote}\r\n`))
+        console.log(chalk.bgRed.white(` ${sad}  WRONG! The correct answer is ${correctNote} \r\n`))
     }
 
     scale.shift()
@@ -33,7 +36,9 @@ const askQuestion = async () => {
 
     return new Promise((resolve) => {
 
-        rl.question(chalk.bgYellow.black(`What is note ${count+1} of the ${scaleName} major scale?\r\n`), (answer) => {
+        const note = emoji.get('musical_note')
+
+        rl.question(chalk.bgYellow.black(` ${note}  What is note ${count+1} of the ${scaleName} major scale? \r\n`), (answer) => {
             checkAnswer(answer)
             resolve()
         })
@@ -50,7 +55,7 @@ const askQuestion = async () => {
 
     }
 
-    console.log(chalk.bgYellow.black(`You got ${score}/7 correct.`))
+    const medal = emoji.get('medal')
+    console.log(chalk.bgBlue.white(` ${medal}  You got ${score}/7 correct `))
     rl.close();
-
 })()
